6aa467a7c678cb2dcb86de39e7d377a0e0b10c21
Mark Young <Markyjay@gmail.com>
2024-05-11 14:07:06 +0000

    deployment to heroku

--- ashclash/settings.py
+++ ashclash/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-8e(*x#ipkwrfj9tbt)lkzr$*#9qn@_whu!85vauz^-p**um@1r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-markyjay-ashclash-jxaef9z52dc.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = ['ashclash-pp5-8ef04402753f.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -117,13 +118,17 @@ WSGI_APPLICATION = 'ashclash.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
