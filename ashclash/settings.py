cf2a61a6e4a3d93c877bae96589c7b8bdcfc1570
Mark Young <Markyjay@gmail.com>
2024-06-08 11:51:38 +0000

    add runtime.txt to fix keyfile argument error for heroku

--- ashclash/settings.py
+++ ashclash/settings.py
@@ -200,7 +200,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'archerstown51@gmail.com'
+    DEFAULT_FROM_EMAIL = 'ashclash.website@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
